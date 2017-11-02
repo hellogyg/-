4bca2db5ebdb4ede52d775afb5403fc4090b0d73
hellogyg <gyghello@163.com>
2017-11-02 15:22:51 +0800

    add 7_game and hello

---
+++ 7_game/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.ListBox2 = New System.Windows.Forms.ListBox
+        Me.ListBox3 = New System.Windows.Forms.ListBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Button5 = New System.Windows.Forms.Button
+        Me.Button6 = New System.Windows.Forms.Button
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(106, 229)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "<"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(106, 258)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = ">"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(300, 239)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "<"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(300, 268)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = ">"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(72, 81)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(100, 124)
+        Me.ListBox1.TabIndex = 4
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 12
+        Me.ListBox2.Location = New System.Drawing.Point(187, 202)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(100, 124)
+        Me.ListBox2.TabIndex = 5
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 12
+        Me.ListBox3.Location = New System.Drawing.Point(300, 81)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(100, 124)
+        Me.ListBox3.TabIndex = 6
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(425, 48)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "汉诺塔，古印度游戏。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "三根柱，n个不同大小圆盘，开始时所有盘叠在一根柱上，当所有盘按规定全部" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "移动到另一根柱上就完成，中间一根柱作辅助。规定：一次只能移动一个" & _
+            "盘，大" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "盘不能叠在小盘上。" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(201, 97)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 8
+        Me.Button5.Text = "<"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(201, 126)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 9
+        Me.Button6.Text = ">"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 102)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 12)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Label2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(454, 338)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox3 As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ 7_game/Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Dim num As Integer = 0
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim n As Integer = Val(InputBox("请输入层数"))
+        For i As Integer = 1 To n
+            Dim item As String = ""
+            For k As Integer = n - i To 0 Step -1
+                item = item & Space(1)
+            Next
+            For j As Integer = 1 To -1 + 2 * i
+                item = item & CStr(i)
+            Next
+            ListBox1.Items.Add(item)
+        Next
+        'ListBox1.Items.Add("     1")
+        'ListBox1.Items.Add("    222")
+        'ListBox1.Items.Add("   33333")
+        'ListBox1.Items.Add("  4444444")
+        'ListBox1.Items.Add(" 555555555")
+        'ListBox1.Items.Add("66666666666")
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If ListBox1.Items.Count > 0 Then
+            If ListBox2.Items.Count = 0 Then
+                ListBox2.Items.Insert(0, ListBox1.Items(0))
+                ListBox1.Items.Remove(ListBox1.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox1.Items(0) < ListBox2.Items(0) Then
+                    ListBox2.Items.Insert(0, ListBox1.Items(0))
+                    ListBox1.Items.Remove(ListBox1.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If ListBox2.Items.Count > 0 Then
+            If ListBox3.Items.Count = 0 Then
+                ListBox3.Items.Insert(0, ListBox2.Items(0))
+                ListBox2.Items.Remove(ListBox2.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox2.Items(0) < ListBox3.Items(0) Then
+                    ListBox3.Items.Insert(0, ListBox2.Items(0))
+                    ListBox2.Items.Remove(ListBox2.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If ListBox2.Items.Count > 0 Then
+            If ListBox1.Items.Count = 0 Then
+                ListBox1.Items.Insert(0, ListBox2.Items(0))
+                ListBox2.Items.Remove(ListBox2.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox2.Items(0) < ListBox1.Items(0) Then
+                    ListBox1.Items.Insert(0, ListBox2.Items(0))
+                    ListBox2.Items.Remove(ListBox2.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If ListBox3.Items.Count > 0 Then
+            If ListBox2.Items.Count = 0 Then
+                ListBox2.Items.Insert(0, ListBox3.Items(0))
+                ListBox3.Items.Remove(ListBox3.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox3.Items(0) < ListBox2.Items(0) Then
+                    ListBox2.Items.Insert(0, ListBox3.Items(0))
+                    ListBox3.Items.Remove(ListBox3.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If ListBox1.Items.Count > 0 Then
+            If ListBox3.Items.Count = 0 Then
+                ListBox3.Items.Insert(0, ListBox1.Items(0))
+                ListBox1.Items.Remove(ListBox1.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox1.Items(0) < ListBox3.Items(0) Then
+                    ListBox3.Items.Insert(0, ListBox1.Items(0))
+                    ListBox1.Items.Remove(ListBox1.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If ListBox3.Items.Count > 0 Then
+            If ListBox1.Items.Count = 0 Then
+                ListBox1.Items.Insert(0, ListBox3.Items(0))
+                ListBox3.Items.Remove(ListBox3.Items(0))
+            Else
+                'If ListBox1.Items(0) > ListBox1.Items(0) Then
+                'MsgBox("大数不能在小数上")
+                If ListBox3.Items(0) < ListBox1.Items(0) Then
+                    ListBox1.Items.Insert(0, ListBox3.Items(0))
+                    ListBox3.Items.Remove(ListBox3.Items(0))
+                Else
+                    MsgBox("大数不能放在小数上")
+                End If
+            End If
+        Else
+            MsgBox("没有什么可以移了")
+        End If
+        num += 1
+        Label2.Text = num
+    End Sub
+End Class
